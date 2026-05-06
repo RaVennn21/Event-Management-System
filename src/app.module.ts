2839d04e061e1ba8c800fffd3cd47bc958d0aef2
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-05-06 23:44:31 +0700

    project structure fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsService } from './application/services/events/events.service';
+import { BookingsService } from './application/services/bookings/bookings.service';
+import { PaymentService } from './application/services/payment/payment.service';
+import { PaymentGatewayService } from './infrastructure/external/payment-gateway/payment-gateway.service';
+import { NotificationService } from './infrastructure/external/notification/notification.service';
+import { EventsController } from './presentation/controllers/events/events.controller';
+import { BookingsController } from './presentation/controllers/bookings/bookings.controller';
+import { RefundsController } from './presentation/controllers/refunds/refunds.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EventsController, BookingsController, RefundsController],
+  providers: [AppService, EventsService, BookingsService, PaymentService, PaymentGatewayService, NotificationService],
 })
 export class AppModule {}
