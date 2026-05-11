81c5a18a453e0b707b2919d503e722a7768fb5f2
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-05-11 22:33:42 +0700

    chore: reset to nest project init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventsService } from './application/services/events/events.service';
-import { BookingsService } from './application/services/bookings/bookings.service';
-import { PaymentService } from './application/services/payment/payment.service';
-import { PaymentGatewayService } from './infrastructure/external/payment-gateway/payment-gateway.service';
-import { NotificationService } from './infrastructure/external/notification/notification.service';
-import { EventsController } from './presentation/controllers/events/events.controller';
-import { BookingsController } from './presentation/controllers/bookings/bookings.controller';
-import { RefundsController } from './presentation/controllers/refunds/refunds.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, EventsController, BookingsController, RefundsController],
-  providers: [AppService, EventsService, BookingsService, PaymentService, PaymentGatewayService, NotificationService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/presentation/controllers/refunds/refunds.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('refunds')
-export class RefundsController {}
